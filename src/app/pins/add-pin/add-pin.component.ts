f71f4efa9acd795e40d798f1bba758f6f48ad144
Vishnu KN <vishnunagaraj7@gmail.com>
2024-04-27 22:34:02 +0530

    Drag and drop image upload functionality done

--- src/app/pins/add-pin/add-pin.component.ts
+++ src/app/pins/add-pin/add-pin.component.ts
@@ -3,6 +3,10 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { NgForm } from '@angular/forms';
 import { PinService } from '../../services/pin.service';
 import { CustomerService } from '../../services/customer.service';
+import { FileUploader } from 'ng2-file-upload';
+
+const URL = 'http://localhost:3000/api/';
+const ImageBaseUrl = 'http://localhost:3000/images/';
 
 @Component({
   selector: 'app-add-pin',
@@ -18,11 +22,28 @@ export class AddPinComponent implements OnInit {
   public ngxValue: any = [];
   public ngxDisabled = false;
 
+  uploader: FileUploader = new FileUploader({ url: URL, autoUpload: true });
+  hasBaseDropZoneOver: boolean;
+  hasAnotherDropZoneOver: boolean;
+  response: string[] = [];
+
+  uploadedImages: string[] = [];
   constructor(
     private activeModal: NgbActiveModal,
     private pinService: PinService,
     private customerService: CustomerService
-  ) {}
+  ) {
+    this.hasBaseDropZoneOver = true;
+    this.hasAnotherDropZoneOver = false;
+
+    this.uploader.response.subscribe((res: string) => {
+      const respObj: string[] = JSON.parse(res);
+      console.log('uploader res', respObj, typeof respObj);
+      respObj.map((x) => this.uploadedImages.push(ImageBaseUrl + x));
+      console.log(this.uploadedImages);
+      this.response = respObj;
+    });
+  }
 
   ngOnInit(): void {
     this.customers = this.customerService.getCustomers().map((customer) => {
@@ -34,6 +55,15 @@ export class AddPinComponent implements OnInit {
     const pin = this.addPinForm.value;
     this.pinService.createPin(pin);
     this.activeModal.close();
+  }
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileDropBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+    this.uploader.uploadAll();
   }
 
   close() {
