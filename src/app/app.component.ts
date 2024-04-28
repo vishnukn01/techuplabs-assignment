d2dbc92245636e2f71b81c5b85d6e7b567ae7c54
Vishnu KN <vishnunagaraj7@gmail.com>
2024-04-28 07:54:49 +0530

    Added alerts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CreateCustomerComponent } from './customers/create-customer/create-customer.component';
 import { AddPinComponent } from './pins/add-pin/add-pin.component';
+import { CustomerService } from './services/customer.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isCustomerModalOpen = false;
   isAddPinModalOpen = false;
+  customerCreated: boolean = false;
+  dismissAlert = false;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private customerService: CustomerService
+  ) {}
+
+  ngOnInit(): void {
+    this.customerService.customerCreated$.subscribe((data) => {
+      this.customerCreated = data;
+    });
+  }
+
+  closeAlert() {
+    this.dismissAlert = true;
+  }
 
   openCustomerModal() {
     this.modalService.open(CreateCustomerComponent);
